4a08b40782f3b9fcc0681049dda5d5c729b4959c
Ercan Öztürk <ercanozturk788@gmail.com>
2024-08-26 13:11:57 +0300

    FS demo

---
+++ fsDemo.js
@@ -0,0 +1,34 @@
+import fs from "fs/promises";
+const readFile = async () => {
+  try {
+    const data = await fs.readFile("./test.txt", "utf8");
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// writeFile()
+
+const writeFile = async () => {
+  try {
+    await fs.writeFile("./test.txt", "Hello, I am writing to this file");
+    console.log("File written to...");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// appendFile()
+const appendFile = async () => {
+  try {
+    await fs.appendFile("./test.txt", "\nThis is appended text");
+    console.log("File appended to...");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+writeFile();
+appendFile();
+readFile();
